9b55a2f9c2e8a7f319e4c95be9bd96b8cee9ee97
August Conwell <august.conwell@gmail.com>
2017-06-28 23:43:36 -0400

    Initial commit

---
+++ app/components/Main.js
@@ -0,0 +1,26 @@
+// Include React
+import React from 'react'
+
+// Import components
+
+
+// Create Main component
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+    
+            </div>
+        )
+    }
+}
+
+// Export the component back for use in other files
+export default Main
