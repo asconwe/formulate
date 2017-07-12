c5c3b2fa525f90cce84ff0797506dc6d763b8d63
August Conwell <august.conwell@gmail.com>
2017-07-12 00:23:08 -0400

    Refactoring FormBuilder

--- app/components/children/FormBuilder.js
+++ app/components/children/FormBuilder.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
+import FormElement from './formBuilder-children/FormElement'
+
 class FormBuilder extends React.Component {
     constructor() {
         super();
         this.state = {
-            formTitle: startingTitle,
+            formTitle: "Your form title",
             elements: []
         }
-        this.handleChange = this.handleChange.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
         this.newElementInPlace = this.newElementInPlace.bind(this);
         this.editElementInPlace = this.editElementInPlace.bind(this);
-        this.handleSave = this.handleSave.bind(this);
+        // this.handleSave = this.handleSave.bind(this);
     }
 
     componentDidMount() {
@@ -22,11 +24,59 @@ class FormBuilder extends React.Component {
             })
         }
     }
-    
+
+    newElementInPlace(index, element) {
+        const newElementsArray = this.state.elements.slice(0, index)
+            .concat({ elementType: element })
+            .concat(this.state.elements.slice(index));
+        this.setState({
+            elements: newElementsArray
+        });
+    }
+
+    editElementInPlace(index, elementContent) {
+        const newElement = this.state.elements[index];
+        const keys = Object.keys(elementContent);
+        keys.map((key) => {
+            newElement[key] = elementContent[key];
+        })
+        const newElementsArray = this.state.elements.slice(0, index)
+            .concat(newElement)
+            .concat(this.state.elements.slice(index + 1));
+        this.setState({
+            elements: newElementsArray
+        });
+        console.log('after edit', console.log(this.state));
+    }
+
+    handleTitleChange(event) {
+        this.setState({
+            formTitle: event.target.value
+        })
+    }
+
     render() {
+        const whiteBackground = {
+            background: "#fff"
+        }
         return (
-            <div>
-                
+            <div className="row">
+                <div style={whiteBackground} className="bordered rounded col-sm-12 col-md-10 col-md-offset-1">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h1 contentEditable onChange={this.handleTitleChange}>{this.state.formTitle}</h1>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="row">
+                                <FormElement size="6" />
+                                <FormElement size="6" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- app/components/children/formBuilder-children/FormElement.js
+++ app/components/children/formBuilder-children/FormElement.js
@@ -1,11 +1,95 @@
 import React, { Component } from 'react';
 
+const horizontal = {
+    width: "100%",
+    height: "15px"
+}
+
+const vertical = {
+    width: "15px",
+    height: "100%",
+    flex: "0 0 15px",
+    margin: 0,
+    padding: 0
+}
+
+const edge = {
+    boxSizing: "border-box",
+    border: "solid 1px grey"
+}
+
+const content = {
+    flexGrow: 1
+}
+
+const container = {
+    width: "100%",
+    display: "flex",
+    xJustifyFontent: "space-around",
+    xAlignItems: "stretch",
+    maxWidth: "1200px"
+}
+
 class FormElement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            size: "6"
+        }
+        this.hoverIn = this.hoverIn.bind(this);
+        this.hoverOut = this.hoverOut.bind(this);
+        this.enter = this.enter.bind(this);
+        this.exit = this.exit.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            size: this.props.size
+        })
+    }
+
+
+    hoverIn() {
+        console.log("in")
+        this.setState({
+            active: true
+        })
+    }
+
+    hoverOut() {
+        this.setState({
+            active: false
+        })
+    }
+
+    enter(event) {
+        if (this.state.active) {
+            console.log(event.target);
+        }
+    }
+
+    exit() {
+
+    }
+
+    handleDrag(event) {
+        console.log(event.pageX)
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+                <div className={`col-sm-12 col-md-${this.state.size}`} onMouseEnter={this.hoverIn} onMouseLeave={this.hoverOut}>
+                    <div style={container} className="row">
+                        <div draggable="true" data-position="top" style={Object.assign({}, horizontal, edge)} onDrag={this.handleDrag} onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12"></div>
+                        <div style={container} onMouseEnter={this.enter} onMouseLeave={this.exit}>
+                            <div data-position="left" style={Object.assign({}, vertical, edge)} onDrag={this.handleDrag} ></div>
+                            <div style={content}>content</div>
+                            <div data-position="right" style={Object.assign({}, vertical, edge)} onDrag={this.handleDrag} ></div>
+                        </div>
+                        <div data-position="bottom" style={Object.assign({}, horizontal, edge)} onDrag={this.handleDrag}  onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12"></div>
+                    </div>
+                </div>
         );
     }
 }
