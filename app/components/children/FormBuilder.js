c11fe9407897107e5f392b925ce8303ea4fcec9a
August Conwell <august.conwell@gmail.com>
2017-07-11 22:08:17 -0400

    reset

--- app/components/children/FormBuilder.js
+++ app/components/children/FormBuilder.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
-class FormBuilder extends Component {
+class FormBuilder extends React.Component {
     constructor() {
-        
+        super();
+        this.state = {
+            formTitle: startingTitle,
+            elements: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.newElementInPlace = this.newElementInPlace.bind(this);
+        this.editElementInPlace = this.editElementInPlace.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }
+
+    componentDidMount() {
+        if (this.props.match.params.index) {
+            const { formTitle, elements } = this.props.getFormToEdit(this.props.match.params.index);
+            this.setState({
+                formTitle,
+                elements
+            })
+        }
+    }
+    
     render() {
         return (
             <div>
