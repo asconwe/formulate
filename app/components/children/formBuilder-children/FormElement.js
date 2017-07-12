d4ce86ab7139158b0cbc65a8f0dd420a5d7c9107
August Conwell <august.conwell@gmail.com>
2017-07-12 17:51:09 +0000

    Reset formbuilder style

--- app/components/children/formBuilder-children/FormElement.js
+++ app/components/children/formBuilder-children/FormElement.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 const horizontal = {
     width: "100%",
-    height: "15px"
+    height: "15px",
+    margin: 0,
+    padding: 0,
+    display: "flex",
+    xJustifyFontent: "space-around",
+    xAlignItems: "stretch",
+    maxWidth: "1200px"
 }
 
 const vertical = {
@@ -15,19 +21,29 @@ const vertical = {
 
 const edge = {
     boxSizing: "border-box",
-    border: "solid 1px grey"
 }
 
 const content = {
     flexGrow: 1
 }
 
-const container = {
+const middle = {
     width: "100%",
     display: "flex",
     xJustifyFontent: "space-around",
     xAlignItems: "stretch",
-    maxWidth: "1200px"
+    maxWidth: "1200px",
+    background: "#edecff",
+}
+
+const container = Object.assign({}, middle, { marginBottom: "10px" });
+
+const activeStyle = {
+    background: "grey"
+}
+
+const inactiveStyle = {
+    background: "none"
 }
 
 class FormElement extends Component {
@@ -35,7 +51,11 @@ class FormElement extends Component {
         super(props);
         this.state = {
             active: false,
-            size: "6"
+            size: "6",
+            top: inactiveStyle,
+            right: inactiveStyle,
+            bottom: inactiveStyle,
+            left: inactiveStyle
         }
         this.hoverIn = this.hoverIn.bind(this);
         this.hoverOut = this.hoverOut.bind(this);
@@ -64,30 +84,39 @@ class FormElement extends Component {
     }
 
     enter(event) {
-        if (this.state.active) {
-            console.log(event.target);
-        }
+        console.log(event.target.dataset.position)
+        const newState = {};
+        newState[event.target.dataset.position] = activeStyle;
+        this.setState( newState );
     }
 
-    exit() {
-
+    exit(event) {
+        const newState = {};
+        newState[event.target.dataset.position] = inactiveStyle;
+        this.setState( newState );
     }
 
-    handleDrag(event) {
-        console.log(event.pageX)
-    }
+  
 
     render() {
         return (
                 <div className={`col-sm-12 col-md-${this.state.size}`} onMouseEnter={this.hoverIn} onMouseLeave={this.hoverOut}>
                     <div style={container} className="row">
-                        <div draggable="true" data-position="top" style={Object.assign({}, horizontal, edge)} onDrag={this.handleDrag} onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12"></div>
-                        <div style={container} onMouseEnter={this.enter} onMouseLeave={this.exit}>
-                            <div data-position="left" style={Object.assign({}, vertical, edge)} onDrag={this.handleDrag} ></div>
-                            <div style={content}>content</div>
-                            <div data-position="right" style={Object.assign({}, vertical, edge)} onDrag={this.handleDrag} ></div>
+                        <div data-position="top" style={Object.assign({}, horizontal, edge, this.state.top)} onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12">
+                            <div data-position="top" style={Object.assign({}, vertical, edge, this.state.left)}></div>
+                            <div data-position="top" style={content}></div>
+                            <div data-position="top" style={Object.assign({}, vertical, edge, this.state.right)}></div>
                         </div>
-                        <div data-position="bottom" style={Object.assign({}, horizontal, edge)} onDrag={this.handleDrag}  onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12"></div>
+                        <div style={middle}>
+                            <div data-position="left" style={Object.assign({}, vertical, edge, this.state.left)} onMouseEnter={this.enter} onMouseLeave={this.exit}></div>
+                            <div style={content}>content</div>
+                            <div data-position="right" style={Object.assign({}, vertical, edge, this.state.right)} onMouseEnter={this.enter} onMouseLeave={this.exit}></div>
+                        </div>
+                        <div data-position="bottom" style={Object.assign({}, horizontal, edge, this.state.bottom)} onMouseEnter={this.enter} onMouseLeave={this.exit} className="col-sm-12">
+                            <div data-position="bottom" style={Object.assign({}, vertical, edge, this.state.left)}></div>
+                            <div data-position="bottom" style={content}></div>
+                            <div data-position="bottom" style={Object.assign({}, vertical, edge, this.state.right)}></div>
+                        </div>
                     </div>
                 </div>
         );
