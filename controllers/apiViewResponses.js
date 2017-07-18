b672466bf8599587979fd258c4776cb948cefb1c
August Conwell <august.conwell@gmail.com>
2017-07-18 22:15:00 +0000

    Count words

--- controllers/apiViewResponses.js
+++ controllers/apiViewResponses.js
@@ -1,6 +1,6 @@
 const PublishedForm = require('../models/PublishedForm');
 
-const { separateWords, countWords } = require('./helpers/analyzeResponse');
+const countWords = require('./helpers/analyzeResponse');
 
 module.exports = (app) => {
     app.get('/api/responses/:id', (req, res) => {
@@ -8,16 +8,20 @@ module.exports = (app) => {
             return res.status(401).json({ success: false });
         }
         const id = req.params.id;
-        PublishedForm.findOne({ refId: id }, (err, response) => {
+        PublishedForm.findOne({ refId: id }, (err, form) => {
             if (err) return res.status(500).json({
                 success: false,
                 message: `Sorry, we couldn't find that form. Please try again.`
             });
-            const wordArr = separateWords(response.responses);
+            const wordArr = form.responses.reduce((a, b) => {
+                return a.concat(b);
+            });
+            console.log(wordArr);
             const wordCounts = countWords(wordArr);
+            const aggregateResponse = Object.assign({}, form, wordCounts);
             return res.status(200).json({
                 success: true,
-                outsiderResponses: response
+                outsiderResponses: aggregateResponse
             });
         });
     });
