e6a77f9f8fe34020668e564218cb756c2001a0d8
August Conwell <august.conwell@gmail.com>
2017-07-20 16:01:57 +0000

    Updated home component to look cleaner

--- app/components/children/Home.js
+++ app/components/children/Home.js
@@ -14,11 +14,20 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div className="center">
-               
-                <br />
-                <Link to='/login'><button className="home-button">Log in</button></Link><br />
-                <Link to='/signup'><button className="home-button">Sign up</button></Link>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-10 col-md-offset-1">
+                        <Link to='/login'><button className="home-button">Log in</button></Link>
+                        <Link to='/signup'><button className="home-button">Sign up</button></Link>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12 col-md-10 col-md-offset-1">
+                        <hr/>
+                        <h2 id="form-builder-response-manager">Form builder, response manager</h2>
+                        <p>Formulate is a streamlined form-builder and response-manager, designed to make the process of sharing surveys or soliciting applications a breeze for everyone involved. Organizations can easily build custom forms and view, manage, and analyze responses, while, on the other end, users just fill out and click submit. Gone are the days of the emailed PDF that the user forgot to save before they sent back.</p>
+                    </div>
+                </div>
             </div>
         )
     }
