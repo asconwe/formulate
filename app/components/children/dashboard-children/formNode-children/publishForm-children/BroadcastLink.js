deacdb283405f6a4c699ab494b26394add000fe5
August Conwell <august.conwell@gmail.com>
2017-07-22 00:02:39 -0400

    Corrected link

--- app/components/children/dashboard-children/formNode-children/publishForm-children/BroadcastLink.js
+++ app/components/children/dashboard-children/formNode-children/publishForm-children/BroadcastLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const base = 'https://formulate-174212' // 'http://localhost:3000' : 'https://formulate-fyi.herokuapp.com';
+const base = 'https://formulate-174212.appspot.com' // 'http://localhost:3000' : 'https://formulate-fyi.herokuapp.com';
 
 class BroadcastLink extends React.Component {
     constructor() {
