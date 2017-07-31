c2c2ae8a979f5572e9c9e291e2e1ef1d93d3fc3f
August Conwell <august.conwell@gmail.com>
2017-07-31 19:57:35 +0000

    Edited logo

--- app/components/children/Heading.js
+++ app/components/children/Heading.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 const logoClass = {
     float: "left",
-    maxWidth: "33px",
-    padding: "1px",
-    margin: '1px 0 0 0',
+    maxWidth: "36.3px",
+    padding: "0px",
+    margin: '0 0 0 0',
     top: "0px",
+    left: "0px",
     position: "absolute",
-    background: "white",
+    background: "none",
 }
 
 class Heading extends React.Component {
@@ -26,10 +27,10 @@ class Heading extends React.Component {
     render() {
         return (
             <header>
+                <a href="#"><img className="logo" style={logoClass} src="/logo.svg" /></a>
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12 col-md-10 col-md-offset-1">
-                            <a href="#"><img className="logo" style={logoClass} src="/logosmall.svg" /></a>
                             {this.props.loggedIn ? <span>
                                 <Link style={{float: 'right'}} to="/dashboard" className="button">Dashboard</Link>
                                 <a href="#" className="button" style={{float: 'right'}} onClick={this.handleLogout}>Logout</a>
